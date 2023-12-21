e320318b7b6bb3746967a53c6f4d99af1810bf18
Theagns <markus.agnsgaard@hotmail.com>
2023-12-21 12:23:42 +0100

    notification email sender gmail

--- src/main/java/com/mtgo/exam/notificationservice/consumer/MessageListener.java
+++ src/main/java/com/mtgo/exam/notificationservice/consumer/MessageListener.java
@@ -1,19 +1,28 @@
 package com.mtgo.exam.notificationservice.consumer;
 
-
+import com.mtgo.exam.notificationservice.EmailService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.annotation.RabbitListeners;
-
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class MessageListener {
+    private final EmailService emailService;
+
+    public MessageListener(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
     @RabbitListener(queues = MQConfig.ORDER_PLACED_QUEUE)
-    public void listener (OrderPlacedMessage orderPlacedMessage){
-        System.out.println(orderPlacedMessage);
+    public void listener(OrderPlacedMessage orderPlacedMessage) {
         log.info("New Order with Order Number: {} has been placed", orderPlacedMessage.getOrderNumber());
+
+
+        String restaurantEmail = "markus@example.com";
+        String subject = "New Order Placed";
+        String body = "A new order has been placed. Order Number: " + orderPlacedMessage.getOrderNumber();
+
+        emailService.sendEmail(restaurantEmail, subject, body);
     }
 }
-
